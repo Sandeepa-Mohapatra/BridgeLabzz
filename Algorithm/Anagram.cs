41bdaebaab719b5a8c2b9e3ed77c41bd82d8329e
Sandeepa Mohapatra <56950918+Sandeepa-Mohapatra@users.noreply.github.com>
2019-10-31 05:54:40 -0700

    Add files via upload

---
+++ Algorithm/Anagram.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm
+{
+    public class Anagram
+    {
+
+        public void ChkAnagram()
+        {
+            //Taking the input from keyboard
+
+            Console.WriteLine("Enter 1st string");
+            string s = Console.ReadLine();
+            Console.WriteLine("Enter 2st string");
+            string s1 = Console.ReadLine();
+
+            //Converting the string to char Array
+            
+
+            char[] a = s.ToCharArray();
+            char[] a1 = s.ToCharArray();
+           
+       
+            //Sorting arrays
+
+            Array.Sort(a);
+            Array.Sort(a1);
+            s = "";
+            s1 = "";
+            //Storing into string again
+            for(int i=0;i<a.Length;i++)
+            {
+                s += a[i];
+            }
+            for (int i = 0; i < a1.Length; i++)
+            {
+                s1 += a1[i];
+            }
+
+            //Cheaking whether its a anagram or not 
+            Console.WriteLine(s.Contains(s1));
+
+            
+
+        }
+    }
+}
---
+++ Algorithm/Runner.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm
+{
+    class Runner
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the no of the program ");
+            Console.WriteLine("Cheak two string are anagram or not(1)");
+            Console.WriteLine("Find Prime nos between 1 to 1000(2)");
+            Console.WriteLine("Vending Machine(3)");
+            Console.WriteLine("Find Day of the week(4)");
+            Console.WriteLine("Temprature conversion(5)");
+            Console.WriteLine("Mothly Payment(6)");
+
+
+            int p = int.Parse(Console.ReadLine());
+            switch (p)
+            {
+                case 1:
+                    Anagram a = new Anagram();
+                    a.ChkAnagram();
+                    break;
+                case 2:
+                    Prime b = new Prime();
+                    b.ChkPrime();
+                    break;
+                case 3:
+                    VendingMachine c = new VendingMachine();
+                    c.CountNote();
+                    break;
+                case 4:
+                    DayOfWeek d = new DayOfWeek();
+                     int m = int.Parse(args[0]);
+                     int d1 = int.Parse(args[1]);
+                    int y = int.Parse(args[2]);
+                    Console.WriteLine(d.FindDay(d1, m, y));
+                    break;
+                case 5:
+                    TempratureConversion e = new TempratureConversion();
+                    Console.WriteLine("Enter the temprature");
+                    int e1 = int.Parse(Console.ReadLine());
+                    e.Conversion(e1);
+                    break;
+                case 6:
+                    MonthlyPayment f = new MonthlyPayment();
+                    Console.WriteLine("Enter the Principal loan amount");
+                    int p1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter the year");
+                    int y1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter the rate of interest");
+                    double r1 = double.Parse(Console.ReadLine());
+                    Console.WriteLine(f.FindMPayment(p1, y1, r1));
+                    break;
+            }
+        }
+    }
+}
