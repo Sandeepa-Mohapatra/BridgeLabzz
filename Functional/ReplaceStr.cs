b1f9f4178fd04c4ffa13757961d8f912038d6d1d
Sandeepa Mohapatra <56950918+Sandeepa-Mohapatra@users.noreply.github.com>
2019-10-31 05:58:50 -0700

    Add files via upload

---
+++ Functional/ReplaceStr.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Functional
+{
+    class ReplaceStr
+    {
+
+       public void ReplaceS()
+        {
+            string str = "Hello <<UserName>> , How are you";
+            Console.WriteLine("Enter the string to replace");
+            string s = Console.ReadLine();
+            Console.WriteLine(str.Replace("<<UserName>>", s));
+
+        }
+    }
+}
