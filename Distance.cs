164a9fec61e1289aec7da84a3ff88d3aff53b00f
unknown <saisandeepa96@gmail.com>
2019-11-05 05:25:38 -0800

    Functional

---
+++ Distance.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Functional
+{
+    class Distance
+    {
+        public void FindDistance()
+        {
+            double x, y, z, a;
+            Console.WriteLine("Enter first point");
+            x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the 2nd point");
+            y = double.Parse(Console.ReadLine());
+            a = (x * x + y * y);
+            z = Math.Sqrt(a);
+            Console.WriteLine("Distance=" + z);
+
+        }
+    }
+}
---
+++ Factors.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Functional
+{
+    class Factors
+    {
+        public void FindFactors()
+        {
+            Console.WriteLine("Enter a no");
+
+            int n = int.Parse(Console.ReadLine());
+
+            while (n % 2 == 0)
+            {
+                Console.WriteLine(2 + " ");
+                n /= 2;
+            }
+
+
+            for (int i = 3; i <= Math.Sqrt(n); i += 2)
+            {
+
+                while (n % i == 0)
+                {
+                    Console.WriteLine(i + " ");
+                    n /= i;
+                }
+            }
+
+
+            if (n > 2)
+                Console.WriteLine(n);
+        
+        }
+    }
+}
