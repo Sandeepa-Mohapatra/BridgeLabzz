7746b9cd4d87bf9dcebca52776511ab765080c8c
Sandeepa Mohapatra <56950918+Sandeepa-Mohapatra@users.noreply.github.com>
2019-11-06 17:47:42 -0800

    Added new code and comment lines

--- Runner.cs
+++ Runner.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/******************************************************Runner***********************************************************/
+/*It contains the main method from where the execution will start. In this Main() all Data structure Programs are present
+<<Author : Sandeepa Mohapatra>>
+ */
+/***********************************************************************************************************************/
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,13 +13,29 @@ namespace DataStructure
     class Runner
     {
         static void Main(string[] args)
-        { 
-            
-            
-            UnOrderedList a = new UnOrderedList();
-            a.Operation();
-            Node node = new Node();
-           
+        {
+            //User will choose the program  here from the given options
+            Console.WriteLine("Enter the no of the program which you want to run from the given programs");
+            Console.WriteLine("UnSorted List(1)");
+            Console.WriteLine("Ordered List(2)");
+
+
+            int p = int.Parse(Console.ReadLine());
+            switch (p)
+            {
+                case 1:
+                    //The object of the class is created
+                    UnOrderedList a = new UnOrderedList();
+                    //This will call the method to perform the operation 
+                    a.Operation();
+                    break;
+                case 2:
+                    //The object of the class is created
+                    OrderedList b = new OrderedList();
+                    //This will call the method to perform the operation 
+                    b.OperationOL();
+                    break;
+            }
         }
     }
 }
