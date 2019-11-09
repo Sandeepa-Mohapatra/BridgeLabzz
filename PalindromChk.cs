af8c0de26e30d85e82ac9ed7a881825d0a30c1db
Sandeepa Mohapatra <56950918+Sandeepa-Mohapatra@users.noreply.github.com>
2019-11-09 01:33:55 -0800

    Added comment lines

--- PalindromChk.cs
+++ PalindromChk.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿/*************************************Ordered List*********************************************/
+/*construct an algorithm to
+input a string of characters and check whether it is a palindrome.The solution to this problem will
+use a deque to store the characters of the string. We will process the string from left to right and 
+add each character to the rear of the deque.
+<<Author : Sandeepa Mohapatra>>
+ */
+/**********************************************************************************************/
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -9,17 +18,21 @@ namespace DataStructure
         public void Check()
         {
             Utility u = new Utility();
+            //Taking user input
             Console.WriteLine("Enter the string to check");
             String s = Console.ReadLine();
+            //adding the data to the queue
             for (int i = 0; i < s.Length; i++)
             {
                 u.addRear(s[i]);
             }
+            //storing the data  in a string after dequeue
             String s1 = "";
             for (int i = 0; i < s.Length; i++)
             {
                 s1 += u.RevAndPrint();
             }
+            //Checking pelindrome
             if (s == s1)
             {
                 Console.WriteLine("Given string is a polindriome");
