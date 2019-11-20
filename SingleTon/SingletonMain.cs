c48b5e9275323429f486db87243a4beb6f57bedd
Sandeepa-Mohapatra <saisandeepa96@gmail.com>
2019-11-20 12:15:46 +0530

    [Sandeepa] Add. Uploaded Delegate Program

---
+++ SingleTon/SingletonMain.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPtternPrograms.SingleTon
+{
+    class SingletonMain
+    {
+        public void Method()
+        {
+            Console.WriteLine("Enter the no of the program which you wnat to run");
+            Console.WriteLine("Eager Initialization(1)");
+            int n = int.Parse(Console.ReadLine());
+            switch(n)
+            {
+                case 1:
+                    EagerInitialization a = EagerInitialization.Obj;
+                    a.Message("Implemented successfully");
+                    break;
+            }
+        }
+    }
+}
