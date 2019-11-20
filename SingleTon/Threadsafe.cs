5079aa5af9577add5e90e79fb9fa232ade065404
Sandeepa-Mohapatra <saisandeepa96@gmail.com>
2019-11-20 18:10:40 +0530

    [Sandeepa] Add. Uploaded Singletone design pattern programs

---
+++ SingleTon/Threadsafe.cs
@@ -0,0 +1,50 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file=Threadsafe.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="Sandeepa Mohapatra"/>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace DesignPtternPrograms.SingleTon
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    /// <summary>
+    /// Class for Thread safe initialization
+    /// </summary>
+    class Threadsafe
+    {
+        /// <summary>
+        /// The object is initialized to null
+        /// </summary>
+        private static Threadsafe obj = null;
+        /// <summary>
+        /// Prevents a default instance of the <see cref="Threadsafe"/> class from being created.
+        /// </summary>
+        private Threadsafe() { }
+        /// <summary>
+        /// The object is created
+        /// </summary>
+        private static object lockThis = new object();
+        /// <summary>
+        /// Gets the get instance.
+        /// </summary>
+        /// <value>
+        /// The instance.
+        /// </value>
+        public static Threadsafe GetInstance
+        {
+            get
+            {                
+               lock (lockThis)
+               {
+                  if (obj == null)
+                     obj = new Threadsafe();
+
+                  return obj;
+               }
+            }
+        }       
+    }
+}
